502fca81b29ee6d5d1a6aa8a9f31765f38c5bef8
serhattas1 <serhattas91@hotmail.com>
2023-02-25 18:41:00 +0300

    Initial commit

---
+++ src/test/java/com/example/bookcase/BookcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
